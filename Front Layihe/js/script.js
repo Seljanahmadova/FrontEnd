a0a0f29e2834a7e242e5fa96c6dfa0bbbc5242f2
Seljan <55322566+Seljanahmadova@users.noreply.github.com>
2019-12-25 03:06:35 +0400

    Add files via upload
    accordion 50%hazirdi, goruntu ve iconlarin deyismeyi qalib

--- Front Layihe/js/script.js
+++ Front Layihe/js/script.js
@@ -1,5 +1,42 @@
+// Navbar JS
+// alinmadi, hevesden dusmemekcun yarimciq qoydum :D
+
 $(document).ready(function () {
     $("#home").click(function () {
         console.log("yes!")
     })
 })
+
+
+// Accordion JS=>
+
+
+
+
+// let accText = document.getElementsByClassName(".accText")
+
+// $(document).ready(function () {
+//     $(".accTitle").click(function () {
+//         console.log(accText)
+//         // $(".accText").css("display", "block")
+//     })
+// }) JQuery alinmadi, kecek JAVASCRIPTe
+
+
+let accList = document.querySelectorAll(".accTitle");
+let spanTitle = document.querySelector("#spanTitle")
+
+for (let i = 0; i < accList.length; i++) {
+    accList[i].addEventListener("click", function () {
+
+        if (this.nextElementSibling.style.maxHeight == "") {
+            this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + "px";
+            spanTitle.style.background = "red"
+        } else {
+            this.nextElementSibling.style.maxHeight = "";
+        }
+
+    });
+}
+
+// icon-un, background-un deyismesi qaldi.
